09c38557593ba8eef7c8db106fe94e7bd94496cf
Maciej Barć <xgqt@gentoo.org>
2022-05-17 15:44:02 +0200

    private/pkgs/hash.rkt: format
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- src/collector2/private/pkgs/hash.rkt
+++ src/collector2/private/pkgs/hash.rkt
@@ -25,8 +25,7 @@
 
 (require
  racket/contract
- racket/set
- )
+ racket/set)
 
 (provide (all-defined-out))
 
@@ -36,26 +35,18 @@
   (-> (and/c hash? immutable?) list? (and/c hash? immutable?))
   (cond
     [(null? lst)  hsh]
-    [else  (hash-remove-keys (hash-remove hsh (car lst)) (cdr lst))]
-    )
-  )
+    [else  (hash-remove-keys (hash-remove hsh (car lst)) (cdr lst))]))
 
 ;; Returns hash with the elements of HSH for which PRED produces a true value
 ;; The PRED procedure is applied to values of keys in HSH
 (define/contract (hash-filter pred hsh)
-  (-> (-> any/c any/c) (and/c hash? immutable?)
-      (and/c hash? immutable?))
+  (-> (-> any/c any/c) (and/c hash? immutable?) (and/c hash? immutable?))
   (hash-remove-keys
    hsh
-   (filter (lambda (key) (not (pred (hash-ref hsh key))))
-           (hash-keys hsh))
-   )
-  )
+   (filter (lambda (key) (not (pred (hash-ref hsh key)))) (hash-keys hsh))))
 
 ;; Execute `set-subtract', taking LST as second argument,
 ;; on the value of KEY from HSH hash
 (define/contract (hash-key-set-subtract hsh key lst)
-  (-> (and/c hash? immutable?) any/c list?
-      (and/c hash? immutable?))
-  (hash-update hsh key (lambda (ds) (set-subtract ds lst)) '())
-  )
+  (-> (and/c hash? immutable?) any/c list? (and/c hash? immutable?))
+  (hash-update hsh key (lambda (ds) (set-subtract ds lst)) '()))
